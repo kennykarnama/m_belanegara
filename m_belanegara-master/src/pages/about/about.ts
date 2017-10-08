996b8414ca9000c14e1cd04a5bef0a8ece78d1f1
zafitra <zafit.ra789@gmail.com>
2017-10-08 22:20:28 +0700

    make lagu wajib read from database

--- m_belanegara-master/src/pages/about/about.ts
+++ m_belanegara-master/src/pages/about/about.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'; 
+import {DetailBeritaPage} from '../detail-berita/detail-berita';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +10,34 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+	public listBerita : any = [];
 
+  constructor(public navCtrl: NavController,public http :Http) {
+
+  }
+
+   getDetailBerita(item,str:string){
+
+   this.navCtrl.push(DetailBeritaPage,{
+     data:item,
+     title:str
+   });
+
+    console.log(item);
+  }
+
+  ionViewWillEnter(){
+  	this.loadListBerita();
+  }
+
+  loadListBerita(){
+  	this.http.get('http://www.belanegaraku.id/m/index.php/Berita/fetch_all_news')
+      .map(res => res.json())
+      .subscribe(data => 
+      {
+         this.listBerita = data;   
+               
+      });
   }
 
 }
