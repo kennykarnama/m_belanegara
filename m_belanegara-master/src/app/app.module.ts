8bdc08ce831568076a39821e0209708404b1088b
zafitra <zafit.ra789@gmail.com>
2017-10-08 20:19:29 +0700

    include detail_berita page class

--- m_belanegara-master/src/app/app.module.ts
+++ m_belanegara-master/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import {DetailBeritaPage} from '../pages/detail-berita/detail-berita';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailBeritaPage
   ],
   imports: [
     BrowserModule,
@@ -31,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    DetailBeritaPage
   ],
   providers: [
     StatusBar,
